d8031718eb3f1354930ced9795c4ebc9d430d11e
DoyinDimeji <Oladimeji.Oladoyin@gmail.com>
2018-03-11 04:22:55 +0100

    Add files via upload

--- js/site.js
+++ js/site.js
@@ -1,24 +1,102 @@
 $(document).ready(function(){
+	$("#vimeoSlide").hide();
+
 	$("#thumb1").click(function(){
 		$("#slider1").animate({
-			right:1300,
-			opacity:0.3,
+			left:-1500
 		});
-		$("#slider1").load("vimeo.html");
-		$("#slider1").animate({
-			right:0,
-			opacity:1,
+		$("#vimeoSlide").show();
+		$("#vimeoSlide").animate({
+			left:0
 		});
+		$("#vid2").text("Genre 1");
 	});
-	$("#testClick").click(function(){
+
+	$("#thumb2").click(function(){
 		$("#slider1").animate({
-			right:1300,
-			opacity:0.3,
+			left:-1500
 		});
-		$("#slider1").unload();
+		$("#vimeoSlide").show();
+		$("#vimeoSlide").animate({
+			left:0
+		});
+		$("#vid2").text("Genre 2");
+	});
+
+	$("#thumb3").click(function(){
 		$("#slider1").animate({
-			right:0,
-			opacity:1,
+			left:-1500
 		});
+		$("#vimeoSlide").show();
+		$("#vimeoSlide").animate({
+			left:0
+		});
+		$("#vid2").text("Genre 3");
+	});
+
+	$("#thumb4").click(function(){
+		$("#slider1").animate({
+			left:-1500
+		});
+		$("#vimeoSlide").show();
+		$("#vimeoSlide").animate({
+			left:0
+		});
+		$("#vid2").text("Genre 4");
+	});
+
+	$("#thumb5").click(function(){
+		$("#slider1").animate({
+			left:-1500
+		});
+		$("#vimeoSlide").show();
+		$("#vimeoSlide").animate({
+			left:0
+		});
+		$("#vid2").text("Genre 5");
+	});
+
+	$("#thumb6").click(function(){
+		$("#slider1").animate({
+			left:-1500
+		});
+		$("#vimeoSlide").show();
+		$("#vimeoSlide").animate({
+			left:0
+		});
+		$("#vid2").text("Genre 6");
+	});
+
+	$("#thumb7").click(function(){
+		$("#slider1").animate({
+			left:-1500
+		});
+		$("#vimeoSlide").show();
+		$("#vimeoSlide").animate({
+			left:0
+		});
+		$("#vid2").text("Genre 7");
+	});
+
+	$("#thumb8").click(function(){
+		$("#slider1").animate({
+			left:-1500
+		});
+		$("#vimeoSlide").show();
+		$("#vimeoSlide").animate({
+			left:0
+		});
+		$("#vid2").text("Genre 8");
+	});
+
+	$("#backBtn").click(function(){
+		$("#slider1").animate({
+			left:0
+		});
+		$("#vimeoSlide").animate({
+			left:1110
+		});
+		$("#vimeoSlide").hide();
+		$("#vid2").text("Music Videos");
 	});
 });
